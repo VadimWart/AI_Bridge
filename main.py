e0608c0beb5dfa3361825651ada36a080ac57a7c
VadimWart <wartvadim@gmail.com>
2026-01-09 19:45:28 +0100

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ai-bridge!")
+from fastapi import FastAPI
+from gemini_client import get_answer_from_gemini
+
+app = FastAPI()
+
+@app.get("/requests")
+def get_my_requests():
+    return "Hello, World!"
 
 
-if __name__ == "__main__":
-    main()
+@app.post("/requests")
+def send_prompt(
+    prompt: str               
+):
+    answer = get_answer_from_gemini(prompt)
+    return {"answer": answer}
