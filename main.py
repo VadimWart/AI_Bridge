45848b29ea65d291860362ba03f8205c5f839a2c
VadimWart <wartvadim@gmail.com>
2026-01-16 17:28:16 +0100

    Implement lifespan context manager for FastAPI and enhance request handling

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-from fastapi import Body, FastAPI
+from contextlib import asynccontextmanager
+from fastapi import Body, FastAPI, Request
+from db import Base, engine, get_user_requests
 from gemini_client import get_answer_from_gemini
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(engine)
+
+    # all startup code here beforer the yield
+
+    yield
+
+
+    # all shutdown code here after the yield
+
+app = FastAPI(
+    title="AI Bridge",
+    lifespan = lifespan
+)
 
 @app.get("/requests")
-def get_my_requests():
-    return "Hello, World!"
+def get_my_requests(request: Request):
+    user_ip_address = request.client.host
+    print("User IP Address:", user_ip_address)
+    user_requests = get_user_requests(ip_address=user_ip_address)
+    return user_requests
 
 
 @app.post("/requests")
